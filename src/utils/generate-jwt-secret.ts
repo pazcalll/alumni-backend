6d319141eca868980f5ba37a2b52dcc1b9cf1e55
pazcalll <yazeedarifin@gmail.com>
2024-05-14 01:24:08 +0700

    feat: jwt generation update

--- src/utils/generate-jwt-secret.ts
+++ src/utils/generate-jwt-secret.ts
@@ -2,6 +2,16 @@ import fs from 'fs';
 import crypto from 'crypto';
 
 const jwtSecret = crypto.randomBytes(64).toString('hex');
-fs.writeFileSync('.env', `JWT_SECRET=${jwtSecret}\n`, { flag: 'a' });
+const envVariables = fs.readFileSync('.env', 'utf8').split('\n');
+
+const updatedVariables = envVariables.map(line => {
+    if (line.startsWith('JWT_SECRET=')) {
+        return `JWT_SECRET=${jwtSecret}`;
+    }
+    return line;
+});
+
+fs.writeFileSync('.env', updatedVariables.join('\n'));
+
 
 console.log('JWT_SECRET generated successfully');
