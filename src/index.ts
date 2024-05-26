f4f1e8eead587105bc47cfa6a5755a65bd40c5cb
pazcalll <yazeedarifin@gmail.com>
2024-05-26 23:49:25 +0700

    feat: serve static images

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 import apiRouter from './routes/api';
-import multer from 'multer';
 import { xssProtection } from './utils/helper';
 
 const app = express();
@@ -9,6 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use('/storage', express.static('storage'))
 app.use('/api', xssProtection, apiRouter.export());
 
 export {app, port};
