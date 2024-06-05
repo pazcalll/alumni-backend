553f0e48f0801768cc2d20ef89b727d03f78e5f6
pazcalll <yazeedarifin@gmail.com>
2024-06-05 23:11:11 +0700

    feat: cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import apiRouter from './routes/api';
 import { xssProtection } from './utils/helper';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
